b0bacc8f28d24de206372e0f529fea40e50151c1
bombies <xjamesdzn@gmail.com>
2023-07-12 12:23:20 -0400

    Remove unneeded files

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,5 +13,6 @@ export const config = {
         "/inventory/:path*",
         "/management/:path*",
         "/account/:path*",
+        "/invoices/:path*",
     ]
 };
--- src/utils/forms/FormUtils.ts
+++
@@ -1,7 +0,0 @@
-export const RequiredField = (val: unknown, errorCb: () => void) => {
-    if (val)
-        return undefined;
-    return errorCb();
-};
-// @ts-ignore
-export const NumberOnlyField = (val: unknown, fieldName: string) => (isNaN(val) ? `${fieldName} must be a number!` : undefined);
