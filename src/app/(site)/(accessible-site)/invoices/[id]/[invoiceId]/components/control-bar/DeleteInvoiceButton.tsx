52ae1b706a566d916038b07245aeaa64669b51d7
bombies <xjamesdzn@gmail.com>
2023-07-12 19:04:31 -0400

    Implement specific invoice deletion

--- src/app/(site)/(accessible-site)/invoices/[id]/[invoiceId]/components/control-bar/DeleteInvoiceButton.tsx
+++ src/app/(site)/(accessible-site)/invoices/[id]/[invoiceId]/components/control-bar/DeleteInvoiceButton.tsx
@@ -1,5 +1,13 @@
+"use client";
+
 import GenericButton from "../../../../../../../_components/inputs/GenericButton";
 import { Invoice } from "@prisma/client";
+import axios from "axios";
+import useSWRMutation from "swr/mutation";
+import ConfirmationModal from "../../../../../../../_components/ConfirmationModal";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { sendToast } from "../../../../../../../../utils/Hooks";
 
 type Props = {
     customerId?: string
@@ -7,12 +15,47 @@ type Props = {
     disabled: boolean
 }
 
-export default function DeleteInvoiceButton({customerId, invoice, disabled}: Props) {
+const DeleteInvoice = (customerId?: string, invoiceId?: string) => {
+    const mutator = (url: string) => axios.delete(url);
+    return useSWRMutation(` /api/invoices/customer/${customerId}/invoice/${invoiceId}`, mutator);
+};
+
+export default function DeleteInvoiceButton({ customerId, invoice, disabled }: Props) {
+    const { trigger: triggerDeletion, isMutating: isDeleting } = DeleteInvoice(customerId, invoice?.id);
+    const [modalOpen, setModalOpen] = useState(false);
+    const router = useRouter();
+
     return (
         <>
+            <ConfirmationModal
+                isOpen={modalOpen}
+                setOpen={setModalOpen}
+                accepting={isDeleting}
+                title="Delete Invoice"
+                message="Are you sure you want to delete this invoice?"
+                onAccept={() => {
+                    triggerDeletion()
+                        .then(() => {
+                            sendToast({
+                                description: "You have successfully deleted that invoice!"
+                            });
+                            router.push(`/invoices/${customerId}`);
+                        })
+                        .catch(e => {
+                            console.error(e);
+                            sendToast({
+                                error: e,
+                                description: "There was an error deleting this invoice!"
+                            });
+                        })
+                    ;
+                }}
+            />
             <GenericButton
+                disabled={disabled}
                 color="danger"
                 variant="flat"
+                onPress={() => setModalOpen(true)}
             >
                 Delete Invoice
             </GenericButton>
