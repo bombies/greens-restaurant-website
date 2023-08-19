adabfa679e41b3db0a35886640c1f1fd0944f342
bombies <xjamesdzn@gmail.com>
2023-08-19 18:21:55 -0400

    Update amplify config logic for workflow

---
+++ configure-amplify.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+IFS='|'
+
+AWSCLOUDFORMATIONCONFIG="{\
+\"configLevel\":\"project\",\
+\"useProfile\":true,\
+\"profileName\":\"default\",\
+\"accessKeyId\":\"${AWS_ACCESS_KEY_ID}\",\
+\"secretAccessKey\":\"${AWS_SECRET_ACCESS_KEY}\",\
+\"region\":\"us-east-1\"\
+}"
+AMPLIFY="{\
+\"projectName\":\"greensrestaurantwebs\",\
+\"envName\":\"ci\",\
+\"defaultEditor\":\"code\"\
+}"
+FRONTEND="{\
+\"frontend\":\"react\"
+}"
+PROVIDERS="{\
+\"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
+}"
+CODEGEN="{\
+\"generateCode\":true,\
+\"codeLanguage\":\"swift\",\
+\"fileNamePattern\":\"graphql/**/*.graphql\",\
+\"generatedFileName\":\"API.swift\",\
+\"generateDocs\":true,\
+\"maxDepth\":2\
+}"
+
+amplify configure project \
+--amplify $AMPLIFY \
+--frontend $FRONTEND \
+--providers $PROVIDERS \
+--yes
