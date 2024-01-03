28e4cbdcb1faa5e30ff8f130f8adbf711f9a6943
bombies <xjamesdzn@gmail.com>
2024-01-03 12:34:16 -0500

    Update remote patterns

--- next.config.js
+++ next.config.js
@@ -3,8 +3,14 @@ const nextConfig = {
     swcMinify: true,
     images: {
         remotePatterns: [
-            "i.imgur.com",
-            "res.cloudinary.com"
+            {
+                protocol: "https",
+                hostname: "i.imgur.com"
+            },
+            {
+                protocol: "https",
+                hostname: "res.cloudinary.com"
+            }
         ]
     },
     webpack: (config, { isServer, nextRuntime, webpack }) => {
