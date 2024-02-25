10d1f2bff6988f1cf27698bc1f3c2c1a9f4f1309
bombies <xjamesdzn@gmail.com>
2024-02-25 11:36:33 -0500

    Update href for viewing all inventory requests

--- src/app/(site)/(accessible-site)/inventory/requests/[id]/components/SpecificRequestContainer.tsx
+++ src/app/(site)/(accessible-site)/inventory/requests/[id]/components/SpecificRequestContainer.tsx
@@ -100,7 +100,7 @@ const SpecificRequestContainer: FC<Props> = ({ id, isAdmin }) => {
             />
             <Spacer y={12} />
             <div className="default-container max-w-full p-12 phone:px-4">
-                <GoBackButton label="View All Requests" href="/inventory/requests?requests_tab=all_requests" />
+                <GoBackButton label="View All Requests" href="/inventory/requests/all" />
                 <Spacer y={6} />
                 <InventoryRequestedItemsTable
                     stickyHeader
